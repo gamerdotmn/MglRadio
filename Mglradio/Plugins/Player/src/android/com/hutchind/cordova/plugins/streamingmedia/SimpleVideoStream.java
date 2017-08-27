18b60d0fb1a98643657b755c241412f6434aaf4f
app sandwich <appsandwich100@gmail.com>
2017-08-27 11:48:03 +0000

    Changes made by app sandwich (appsandwich100@gmail.com).

--- Mglradio/Plugins/Player/src/android/com/hutchind/cordova/plugins/streamingmedia/SimpleVideoStream.java
+++ Mglradio/Plugins/Player/src/android/com/hutchind/cordova/plugins/streamingmedia/SimpleVideoStream.java
@@ -78,7 +78,8 @@ public class SimpleVideoStream extends Activity implements
                 RelativeLayout.LayoutParams closeLayoutParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
                 closeLayoutParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
                 close.setLayoutParams(closeLayoutParams);
-                
+                closeLayoutParams.width=60;
+                closeLayoutParams.height=60;
                 int closeResId = activityRes.getIdentifier("video_top_back", "drawable", this.getPackageName());
                 Drawable closeIcon = activityRes.getDrawable(closeResId);
                 if(android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.JELLY_BEAN)
